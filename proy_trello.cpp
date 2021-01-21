19e81e757c26b2164a74ba92ec6d666879fb4035
axelcinco <73139260+axelcinco@users.noreply.github.com>
2021-01-20 17:54:37 -0700

    Add files via upload

--- proy_trello.cpp
+++ proy_trello.cpp
@@ -3,6 +3,7 @@
 #include<string.h>
 using namespace std;
 char usuario[30],contra[30],acum_usuario[30],acum_contra[30];
+string pasword,usua;
 
 int longitud=0,contador=0,opc=0;
 void crear_usu()
@@ -30,53 +31,79 @@ int main()
 	cout<<"------------------------------------------------------"<<'\n';
 	cout<<" *****bienvenido a tu administrador de tareas****** "<<'\n';
 	cout<<"------------------------------------------------------"<<'\n';
-    cout<<"digite su usuario: ";cin>>usuario;
-    cout<<"digite su pasword: ";cin>>contra;
-   	
-	
-    if(usuario == "axel" && contra =="1234")
-    {
-    	do{
-    		cout<<"bienvenido usuario "<<usuario<<'\n';
-		    cout<<"digite 1.- si quiere crear una tarea "<<'\n';
-		    cout<<"digite 2.- si quiere craer una subtarea "<<'\n';
-		    cout<<" digite 3.- para ver la realizacion de tareas"<<'\n';
-		    cout<<" digite 4.- para eliminar tarea "<<'\n';
-		    cout<<"digite 5.- para salir "<<'\n';
-		    switch(opc)
-		    {
-		    	 case 1:
-		    	 	break;
-		    	 	
-		    	case 2:
-		    		break;
-		    	
-		    	case 3:
-				break;
-				case 4:
-					break;
-				case 5:
-					break;
-					break;
-				default:
-					cout<<"opcion invalida "<<endl;
-					break;
-		    		
-		    	
-			}//fin del switch
-	
-	
-	}while(opc!=5);		
-
-	}
     
-    	else
-	{
-		 contador=contador+1;
-		cout<<"usuario no encontrado "<<'\n';
+   	while(contador!=10)
+{
+    cout<<"digite su usuario: ";cin>>usua;
+    cout<<"digite su pasword: ";cin>>pasword;
+		 
+		if (usua=="axel" && pasword=="1234")
+		{
+			
+	   system("cls");		
+	do{
+		 contador=0;
 
+		{
+			
+			
+			cout<<"_______________________________";
+			cout<<endl;
+			cout<<"presione 1.- para crear tareas "<<'\n';           //hacemos un menu para el usuario
+			cout<<"presione 2.- para hacer una subtarea "<<'\n';
+			cout<<"presion 3.- para ver resultados de la tarea"<<'\n';
+			cout<<"presione 4.- para eliminar tareas"<<'\n';
+			cout<<"presione 5.- para salir"<<'\n';
+			cout<<"____________________________________"<<'\n';
 		
-	}
+			cout<<"elija una opcion: ";cin>>opc; //leemos la opcion para el menu
+			system("cls");
+			switch(opc)
+			
+			{
+				  
+				case 1:	 
+				break;
+				
+					
+				case 2:	
+				break;
+					
+				case 3:
+			     break;
+						 
+					case 5:
+						cout<<"hasta luego usuario "<<usua;
+					break;
+					break;
+					
+				default:
+					cout<<"opcion invalida"<<endl;
+					
+				break;
+						
+				
+			}   //termina el switch
+			cout<<endl;	
+			system("pause");
+			system("cls");
+		}			
+				
+}while(opc!=5); // termino del ciclo while condicion para que cuando el usuario de mas de 10 intentos se cierre el programa	}while(respuesta!=2);	
+
+}
+	else 
+			{
+				 
+				 contador=contador+1;
+				 	cout<<"  datos erroneos: "<<endl;
+				 	cout<<" ADVERTENCIA, tienes tan solo 10 intentento y llevas "<<contador; 
+				 	cout<<endl;
+					
+}		
+		}
+	
+    
 
 
 	return 0;
